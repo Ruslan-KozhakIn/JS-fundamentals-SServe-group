6151d35a2d58faf5bca751047cc88c0416ade66a
Ruslan-KozhakIn <ruslangeraklyon@gmail.com>
2023-04-14 02:43:26 +0300

    starting coding

---
+++ js/main.js
@@ -0,0 +1,37 @@
+console.log('Kozhakin');
+
+let customColor = "red";
+let globalColor = "green";
+console.log(customColor, globalColor);
+
+
+customColor = "green";
+globalColor = "red";
+
+console.log(customColor, globalColor);
+
+let infoForAll = "This " + "Present " + "for " + "birthday!";
+
+let sizeApp = 45 + 45;
+
+
+let middleWeight = 45 > 110;
+
+
+let highestValue = 195 > 55;
+
+
+let forFree;
+forFree;
+
+
+let techWorkOffer = null;
+
+console.log(`Advert: ${infoForAll}, Size: ${sizeApp}, Weight: ${middleWeight}, Value: ${highestValue} Sale: ${forFree}, Work: ${techWorkOffer}`);
+ 
+let isAdult = confirm("Have you reached the age of 18 ?");
+console.log(isAdult);
+
+
+
+
